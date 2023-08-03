0317ca7203ee9cda77ec2deb23b23d44e821a0bb
aryan626 <140093177+aryan626@users.noreply.github.com>
2023-08-03 09:52:37 +0530

    sketchpy

---
+++ project_02.py
@@ -0,0 +1,7 @@
+'''from sketchpy import library as lib
+obj = lib.apj()
+obj.draw()'''
+
+from sketchpy import canvas
+obj = canvas.sketch_from_image(r'c:\Users\Aryan pandey\Downloads\Firefly a cute monster with a long tongue 1802.jpg')
+obj.draw()
